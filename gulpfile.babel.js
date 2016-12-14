0240391bdf8ee3ce3c8d2790ab895d1f991a0112
fallenoak <github@fallenoak.me>
2016-12-14 13:07:02 -0600

    Revamped tasks
    * Migrated linting to gulp
    
    * Added watch versions of build and test tasks
    
    * Renamed start:dev to develop
    
    * Updated build task to produce test coverage reports
    
    * Fixed issue that caused tests to run multiple times

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,21 +4,26 @@ import cache from 'gulp-cached';
 import del from 'del';
 import gulp from 'gulp';
 import jest from 'gulp-jest';
+import eslint from 'gulp-eslint';
 import nodemon from 'gulp-nodemon';
-import pkg from './package.json';
 import plumber from 'gulp-plumber';
+import util from 'gulp-util';
+import pkg from './package.json';
+
+const paths = {
+  scripts: 'src/**/*.js',
+  tests: 'tests'
+};
 
 const config = {
-  db: new Config(pkg.name),
-  scripts: 'src/**/*.js',
-  tests: 'tests/**/*.js'
+  db: new Config(pkg.name)
 };
 
 const jestDefaults = {
-  rootDir: '.',
   automock: false,
   collectCoverage: false,
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  testRegex: '\\.test\\.js$'
 };
 
 gulp.task('reset', function(done) {
@@ -27,6 +32,54 @@ gulp.task('reset', function(done) {
   done();
 });
 
+gulp.task('lint', function() {
+  return gulp.src(paths.scripts)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('lint:informal', function() {
+  return gulp.src(paths.scripts)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:watch', gulp.series('lint:informal', function(done) {
+  gulp.watch(paths.scripts, gulp.series('lint:informal'));
+  done();
+}));
+
+gulp.task('test', function() {
+  const jestConfig = Object.assign({}, jestDefaults, { collectCoverage: false });
+
+  return gulp.src(paths.tests)
+    .pipe(jest({ config: jestConfig }));
+});
+
+gulp.task('test:coverage', function() {
+  const jestConfig = Object.assign({}, jestDefaults, { collectCoverage: true });
+
+  return gulp.src(paths.tests)
+    .pipe(jest({ config: jestConfig }));
+});
+
+gulp.task('test:informal', function(done) {
+  const jestConfig = Object.assign({}, jestDefaults);
+
+  return gulp.src(paths.tests)
+    .pipe(plumber(function (error) {
+      util.log(`Error: ${error.message}`);
+      done();
+    }))
+    .pipe(jest({ config: jestConfig }));
+});
+
+gulp.task('test:watch', gulp.series('test:informal', function(done) {
+  gulp.watch([paths.scripts, paths.tests], gulp.series('test:informal'));
+  done();
+}));
+
 gulp.task('build:clean', function(cb) {
   del([
     'lib/*',
@@ -34,7 +87,14 @@ gulp.task('build:clean', function(cb) {
 });
 
 gulp.task('build:compile', function() {
-  return gulp.src(config.scripts)
+  return gulp.src(paths.scripts)
+    .pipe(cache('babel'))
+    .pipe(babel())
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('build:compile:informal', function() {
+  return gulp.src(paths.scripts)
     .pipe(cache('babel'))
     .pipe(plumber())
     .pipe(babel())
@@ -42,33 +102,42 @@ gulp.task('build:compile', function() {
 });
 
 gulp.task('build', gulp.series(
-  'build:clean', 'build:compile'
+  'lint', 'test:coverage', 'build:clean', 'build:compile'
 ));
 
-gulp.task('test', function() {
-  const jestConfig = Object.assign({}, jestDefaults, { collectCoverage: false });
-  return gulp.src(config.tests).pipe(jest({ config: jestConfig }));
-});
+gulp.task('build:informal', gulp.series(
+  'lint:informal', 'test:informal', 'build:clean', 'build:compile:informal'
+));
 
-gulp.task('test:coverage', function() {
-  const jestConfig = Object.assign({}, jestDefaults, { collectCoverage: true });
-  return gulp.src(config.tests).pipe(jest({ config: jestConfig }));
-});
+gulp.task('build:refresh', gulp.series(
+  'lint', 'test', 'build:compile'
+));
 
-gulp.task('watch', function(done) {
-  gulp.watch(config.scripts, gulp.series('build:compile', 'test'));
+gulp.task('build:refresh:informal', gulp.series(
+  'lint:informal', 'test:informal', 'build:compile'
+));
+
+gulp.task('build:watch', gulp.series('build:informal', function(done) {
+  gulp.watch([paths.scripts, paths.tests], gulp.series('build:refresh:informal'));
   done();
-});
+}));
 
-gulp.task('start:dev', function(done) {
+gulp.task('develop:load', gulp.series(
+  'lint', 'test', 'build:clean', 'build:compile'
+));
+
+gulp.task('develop:reload', gulp.series(
+  'lint', 'test', 'build:compile'
+));
+
+gulp.task('develop', gulp.series('develop:load', function(done) {
   nodemon({
     script: 'bin/wowser-pipeline',
     watch: ['src'],
-    tasks: ['build:compile', 'test'],
+    tasks: ['develop:reload'],
+    delay: '250'
   });
   done();
-});
+}));
 
-gulp.task('default', gulp.series(
-  'build', 'test', 'watch'
-));
+gulp.task('default', gulp.series('build'));
